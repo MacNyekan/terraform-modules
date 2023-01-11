ca4a14e5b9cee64c2e41a79af1910df013ed5870
Mac Nyekan <macnyekan2@gmail.com>
2023-01-10 20:54:26 -0500

    created iam role

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -35,6 +35,11 @@ module "nat_gateway" {
 module "security_group" {
   source = "../modules/security-groups"
   vpc_id = module.vpc.vpc_id
+}
 
+#create ecs task execution role
+module "ecs_task_execution_role" {
+  source       = "../modules/ecs-task-execution-role"
+  project_name = module.vpc.project_name
 }
 
